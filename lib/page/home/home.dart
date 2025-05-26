a0523bf9e3bd0b4df133781dbf88fa782da95713
unknown <trytusan@gmail.com>
2025-05-26 10:06:10 +0800

    tusan

--- lib/page/home/home.dart
+++ lib/page/home/home.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:plan_ease/page/notification/notification.dart';
 import 'package:plan_ease/page/polling/polling.dart';
-import 'package:plan_ease/page/profile/profile.dart';
 import 'package:plan_ease/page/schedule/schedule.dart';
 import 'package:plan_ease/widget/home/home.dart';
 import 'package:plan_ease/widget/component/appbar.dart';
